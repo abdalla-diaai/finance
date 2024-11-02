5811f08d9f7a0e0735c6c0d905afdc8bd022be25
Abdalla Diaai <abdalla@Abdalla.local>
2024-11-02 19:01:48 +0000

    added some functionality to index page, created table to store user portfolio, need to add more columns to store stock.

---
+++ app.py
@@ -0,0 +1,155 @@
+import os
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+import sqlite3
+from werkzeug.security import check_password_hash, generate_password_hash
+from helpers import apology, login_required, lookup, usd
+
+# Configure application
+app = Flask(__name__)
+
+sql_statements = [ 
+    """CREATE TABLE IF NOT EXISTS portfolio (
+            id INTEGER PRIMARY KEY, 
+            user_id INTEGER NOT NULL,
+            FOREIGN KEY (user_id) REFERENCES users (id) 
+            );"""]
+
+with sqlite3.connect("finance.db") as db:
+        cursor = db.cursor()
+        for statement in sql_statements:
+            cursor.execute(statement)
+        db.commit()
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/", methods=["GET", "POST"])
+@login_required
+def index():
+    """Show portfolio of stocks"""
+     # Query database for username
+    with sqlite3.connect("finance.db") as db:
+        db.row_factory = sqlite3.Row
+        user_data = db.execute("SELECT * FROM users WHERE id = (SELECT user_id FROM portfolio)").fetchall()
+    return render_template("index.html", user_data=user_data[0]["cash"])
+
+@app.route("/buy", methods=["GET", "POST"])
+@login_required
+def buy():
+    """Buy shares of stock"""
+    if request.method == "POST":
+        stock=request.form.get("symbol")
+        shares_number = request.form.get("shares")
+        price = lookup(stock)
+        if price:
+            return render_template("index.html", price=price, shares_number=shares_number)
+        else:
+            return apology("TODO")
+    return render_template("buy.html")
+
+@app.route("/history")
+@login_required
+def history():
+    """Show history of transactions"""
+    return apology("TODO")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        with sqlite3.connect("finance.db") as db:
+            db.row_factory = sqlite3.Row
+            rows = db.execute(
+                "SELECT * FROM users WHERE username = ?", (request.form.get("username"), )
+            ).fetchall()
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in4
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/quote", methods=["GET", "POST"])
+@login_required
+def quote():
+    """Get stock quote."""
+    if request.method == "POST":
+        stock=request.form.get("stock")
+        price = lookup(stock)
+        if price:
+            return render_template("quoted.html", price=price)
+        else:
+            flash(f"Stock symbol does not exist!", category="error")
+    return render_template("quote.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    if request.method == "POST":
+        username = request.form.get("username")
+        hash = generate_password_hash(request.form.get("password"))
+        with sqlite3.connect("finance.db") as db:
+            # handle error in registration
+            try:
+                db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", (username, hash))
+                db.execute("INSERT INTO portfolio (user_id) SELECT id FROM users WHERE username = ?", (username,))
+
+                flash(f"Successfully registered!", category="success")
+            except sqlite3.IntegrityError:
+                flash(f"Username already exists!", category="error")
+    return render_template("register.html")
+
+@app.route("/sell", methods=["GET", "POST"])
+@login_required
+def sell():
+    """Sell shares of stock"""
+    return apology("TODO")
